412cd259579b2fd8c53ebade33cba9e8235dc104
Ankhyang <ankh3690@163.com>
2021-02-18 21:44:10 +0800

    添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,18 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 const router = createRouter({
     history: createWebHashHistory, // hash模式：createWebHashHistory，history模式：createWebHistory
-    
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: () => import('@/views/home'),
+            meta: {
+                index: 1
+            }
+        }
+    ]
 })
