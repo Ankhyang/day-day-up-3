375e8547c9359a54deaf446e0c06bf8f58507826
Ankhyang <ankh3690@163.com>
2021-02-18 18:22:48 +0800

    添加路由

---
+++ src/router/index.js
@@ -0,0 +1,5 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+const router = createRouter({
+    history: createWebHashHistory, // hash模式：createWebHashHistory，history模式：createWebHistory
+    
+})
